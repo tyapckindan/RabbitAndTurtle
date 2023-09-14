58d06b7dfbcfd4ec9f1eeb64c53de2a0d0cbf427
Данила Тяпкин <dtapkin6@gmail.com>
2023-09-14 12:24:56 +0300

    Добавьте файлы проекта.

---
+++ RabbitAndTurtle/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void MyThreadTurtle()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Черепаха проползла " + i);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Thread turtle = new(MyThreadTurtle);
+        turtle.Start();
+
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Заяц пробежал " + i);
+        }
+    }
+}
